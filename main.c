0f9a3b70d4eec5f8621f89f1744b60b0c9c5c470
gboecker24 <g-boecker@onu.edu>
2025-09-23 19:39:04 -0400

    Corb on the cob
    Corb on the cob

--- main.c
+++ main.c
@@ -35,6 +35,30 @@ int main() { printf("Hello World!
     printf(" commit #3");
 
 
+#include <stdio.h>
+
+int main() {
+    printf("        ,--.\n");
+    printf("       ( o  )\n");
+    printf("      /|--|\\\n");
+    printf("     /_|__|_\\\n");
+    printf("      ||  ||     CORB ON THE COB\n");
+    printf("     /_|__|_\\    ~ majestic, crunchy, proud ~\n");
+    printf("    //||||||\\\\\n");
+    printf("   ||||||||||||\n");
+    printf("   ||||||||||||\n");
+    printf("   ||||||||||||\n");
+    printf("   ||||||||||||\n");
+    printf("   ||||||||||||\n");
+    printf("   ||||||||||||\n");
+    printf("   ||||||||||||\n");
+    printf("   ||||||||||||\n");
+    printf("    \\||||||||/\n");
+    printf("     \\______/ \n");
+    return 0;
+}
+
+
 }
 
 // rewoiurowieuroiwuerouwoerwoeurowuerowuer
