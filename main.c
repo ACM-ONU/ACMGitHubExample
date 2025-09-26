23b30457104d229a704dcc1a01b210c95f23ecd1
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-09-25 20:57:37 -0400

    stuff

--- main.c
+++ main.c
@@ -1,93 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <windows.h>
+// #include <windows.h>
 
 int main() {
-    printf("Hello World!\n");
-    printf("Corb I don't know how to write in C, all I know is COBOL.\n\n");
+  printf("Hello World!\n");
+  printf("Corb I don't know how to write in C, all I know is COBOL.\n\n");
 
+  printf("⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+  printf("⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀⠀\n");
+  printf("⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀⠀\n");
+  printf("⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀⠀\n");
+  printf("⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀⠀\n");
+  printf("⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀\n");
+  printf("⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀\n");
+  printf("⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀\n");
+  printf("⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀\n");
+  printf("⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀\n");
+  printf("⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀\n");
+  printf("⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀\n");
+  printf("⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀\n");
+  printf("⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀\n");
+  printf("⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠀⢠⣿⣿⠀⠀⠀\n");
+  printf("⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀\n");
+  printf("⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀\n");
+  printf("⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀\n");
+  printf("⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
+  printf("ah Corbin look Among Us.\n\n");
 
-    printf("⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
-    printf("⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀⠀\n");
-    printf("⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀⠀\n");
-    printf("⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀⠀\n");
-    printf("⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀⠀\n");
-    printf("⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀\n");
-    printf("⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀\n");
-    printf("⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀\n");
-    printf("⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀\n");
-    printf("⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀\n");
-    printf("⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀\n");
-    printf("⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀\n");
-    printf("⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀\n");
-    printf("⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀\n");
-    printf("⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠀⢠⣿⣿⠀⠀⠀\n");
-    printf("⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀\n");
-    printf("⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀\n");
-    printf("⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀\n");
-    printf("⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n");
-    printf("ah Corbin look Among Us.\n\n");
+  // Corb on the Cob ASCII Art
+  printf("        ,--.\n");
+  printf("       ( o  )\n");
+  printf("      /|--|\\\n");
+  printf("     /_|__|_\\\n");
+  printf("      ||  ||     CORB ON THE COB\n");
+  printf("     /_|__|_\\    ~ majestic, crunchy, proud ~\n");
+  printf("    //||||||\\\\\n");
+  printf("   ||||||||||||\n");
+  printf("   ||||||||||||\n");
+  printf("   ||||||||||||\n");
+  printf("   ||||||||||||\n");
+  printf("   ||||||||||||\n");
+  printf("   ||||||||||||\n");
+  printf("   ||||||||||||\n");
+  printf("   ||||||||||||\n");
+  printf("    \\||||||||/\n");
+  printf("     \\______/ \n\n");
 
-    // Corb on the Cob ASCII Art
-    printf("        ,--.\n");
-    printf("       ( o  )\n");
-    printf("      /|--|\\\n");
-    printf("     /_|__|_\\\n");
-    printf("      ||  ||     CORB ON THE COB\n");
-    printf("     /_|__|_\\    ~ majestic, crunchy, proud ~\n");
-    printf("    //||||||\\\\\n");
-    printf("   ||||||||||||\n");
-    printf("   ||||||||||||\n");
-    printf("   ||||||||||||\n");
-    printf("   ||||||||||||\n");
-    printf("   ||||||||||||\n");
-    printf("   ||||||||||||\n");
-    printf("   ||||||||||||\n");
-    printf("   ||||||||||||\n");
-    printf("    \\||||||||/\n");
-    printf("     \\______/ \n\n");
+  // Print CORB lines (shortened for sanity)
+  for (int i = 0; i < 20; i++) {
+    printf("CORB #%d\n", i + 1);
+  }
 
-    // Print CORB lines (shortened for sanity)
-    for (int i = 0; i < 20; i++) {
-        printf("CORB #%d\n", i + 1);
-    }
+  printf("\nI have printed CORB 20 times.\n");
+  printf("Was it useful? No.\n");
+  printf("Was it beautiful? Absolutely.\n");
+  printf("Goodbye.\n\n");
 
-    printf("\nI have printed CORB 20 times.\n");
-    printf("Was it useful? No.\n");
-    printf("Was it beautiful? Absolutely.\n");
-    printf("Goodbye.\n\n");
+  // Corb's thoughts
+  const char *thoughts[] = {"Is corn just grass with ambition?",
+                            "What if I'm the cob and the corn is wearing me?",
+                            "I forgot what I was thinking about.",
+                            "Do birds dream of golden kernels?",
+                            "I once saw a scarecrow blink.",
+                            "Corn... is it crunchy truth or buttery illusion?",
+                            "I am Corb. I am eternal.",
+                            "Why do humans butter their destiny?",
+                            "The cob whispers secrets only I understand.",
+                            "I fear the microwave."};
 
-    // Corb's thoughts
-    const char* thoughts[] = {
-        "Is corn just grass with ambition?",
-        "What if I'm the cob and the corn is wearing me?",
-        "I forgot what I was thinking about.",
-        "Do birds dream of golden kernels?",
-        "I once saw a scarecrow blink.",
-        "Corn... is it crunchy truth or buttery illusion?",
-        "I am Corb. I am eternal.",
-        "Why do humans butter their destiny?",
-        "The cob whispers secrets only I understand.",
-        "I fear the microwave."
-    };
+  int num_thoughts = sizeof(thoughts) / sizeof(thoughts[0]);
+  srand((unsigned int)time(NULL));
 
-    int num_thoughts = sizeof(thoughts) / sizeof(thoughts[0]);
-    srand((unsigned int)time(NULL));
+  for (int i = 0; i < 10; i++) {
+    int index = rand() % num_thoughts;
+    printf("🧠 Corb thinks: %s\n", thoughts[index]);
+    // sleep(1000); // wait 1 second (Windows)
+  }
 
-    for (int i = 0; i < 10; i++) {
-        int index = rand() % num_thoughts;
-        printf("🧠 Corb thinks: %s\n", thoughts[index]);
-        Sleep(1000); // wait 1 second (Windows)
-    }
+  printf("\nCorb has ascended. 🌽✨\n");
 
-    printf("\nCorb has ascended. 🌽✨\n");
-
-    return 0;
+  return 0;
 }
 int add(int a, int b) { return a + b; }
 
 // bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 // aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 //
-// 6767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767 main
+// 6767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767676767
+// main
